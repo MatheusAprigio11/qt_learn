dbbe55756515391341e9623fd244e6b2a54cebbf
Matheus Aprigio <126816425+MatheusAprigio11@users.noreply.github.com>
2023-06-21 14:34:10 -0300

    Update learnpyqt.py

--- testpyqtprjas/learnpyqt.py
+++ testpyqtprjas/learnpyqt.py
@@ -7,23 +7,23 @@ class test:
 
         self.som = None
 
-        self.tl_exemplo = uic.loadUi("./Exemplo.ui")
-        self.tl_exemplo.okBtn.clicked.connect(lambda: self.test())
-        self.tl_exemplo.show()
+        self.tl_exemplo = uic.loadUi("./Exemplo.ui") #loadui para carregar a tela
+        self.tl_exemplo.okBtn.clicked.connect(lambda: self.test())    #lambda é quando voce quer utilizar uma função e precisa parar parametro
+        self.tl_exemplo.show()   #arquivo show serve para abrir a tela
 
         self.tl_2 = uic.loadUi("./2.ui")
         self.tl_2.nextBtn.clicked.connect(self.test2)
 
-        self.player = QtMultimedia.QMediaPlayer()
+        self.player = QtMultimedia.QMediaPlayer()      #aditivos para musica
         self.playlist = QtMultimedia.QMediaPlaylist()
         self.playlist.addMedia(QtMultimedia.QMediaContent(QUrl.fromLocalFile("./scream.mp3")))
         self.playlist.setPlaybackMode(QtMultimedia.QMediaPlaylist.Loop)
         self.player.setPlaylist(self.playlist)
         self.player.play()
 
-        app.exec()
+        app.exec()    #executar a classe
 
-    def test(self):
+    def test(self):     #metodo feito para fechar uma tela e abrir a outra    -    daqui para baixo são funções
         self.tl_exemplo.close()
         self.tl_2.show()
         self.sound()
